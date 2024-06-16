7a5b6f30f37ed870da635a4f75c4064049d01f47
Ana Bagić <ana.bagic13@gmail.com>
2024-06-16 14:56:20 +0200

    Keyboard start

--- code/src/main/java/pianolearn/diplomskirad/view/keyboard/PianoKey.java
+++ code/src/main/java/pianolearn/diplomskirad/view/keyboard/PianoKey.java
@@ -4,5 +4,14 @@ import javafx.scene.shape.Rectangle;
 
 public class PianoKey extends Rectangle {
 
+    private final int keyIndex;
 
+    public PianoKey(double width, double height, int keyIndex) {
+        super(width, height);
+        this.keyIndex = keyIndex;
+    }
+
+    public int getKeyIndex() {
+        return keyIndex;
+    }
 }
--- code/src/main/java/pianolearn/diplomskirad/view/keyboard/PianoKeyboard.java
+++ code/src/main/java/pianolearn/diplomskirad/view/keyboard/PianoKeyboard.java
@@ -1,23 +1,49 @@
 package pianolearn.diplomskirad.view.keyboard;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Rectangle;
-import pianolearn.diplomskirad.view.base.BaseView;
+import javafx.scene.paint.Color;
+import pianolearn.diplomskirad.view.BaseView;
 
 public class PianoKeyboard extends BaseView {
 
-    private static class Key {
-        Rectangle rect;
-        int keyIndex;
+    private final StackPane root = new StackPane();
 
-        public Key(Rectangle rect, int keyIndex) {
-            this.rect = rect;
-            this.keyIndex = keyIndex;
-        }
+    private static final int WHITE_KEY_WIDTH = 10;
+    private static final int WHITE_KEY_HEIGHT = 50;
+    private static final int BLACK_KEY_WIDTH = 6;
+    private static final int BLACK_KEY_HEIGHT = 35;
+
+    public PianoKeyboard(int numberOfKeys) {
+        super();
+        drawKeyboard(numberOfKeys);
     }
 
-    public PianoKeyboard() {
-        super(new StackPane());
+    private void drawKeyboard(int numberOfKeys) {
+        root.getChildren().clear();
+
+        Label label = new Label("kkfkjfhjkdshfjkd");
+        root.getChildren().add(label);
+
+        // Draw white keys
+        for (int i = 0; i < numberOfKeys; i++) {
+            PianoKey whiteKey = new PianoKey(WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT, i);
+            whiteKey.setFill(Color.WHITE);
+            whiteKey.setStroke(Color.BLACK);
+            whiteKey.setX(i * WHITE_KEY_WIDTH);
+            root.getChildren().add(whiteKey);
+        }
+
+        // Draw black keys
+        for (int i = 0; i < numberOfKeys; i++) {
+            if (i % 7 != 2 && i % 7 != 6) { // Skip where there are no black keys
+                PianoKey blackKey = new PianoKey(BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT, i);
+                blackKey.setFill(Color.BLACK);
+                blackKey.setX(i * WHITE_KEY_WIDTH + (WHITE_KEY_WIDTH - BLACK_KEY_WIDTH / 2));
+                root.getChildren().add(blackKey);
+            }
+        }
     }
 
     @Override
@@ -28,5 +54,10 @@ public class PianoKeyboard extends BaseView {
     @Override
     protected void setupConstraints() {
 
+    }
+
+    @Override
+    public Pane getRoot() {
+        return root;
     }
 }
