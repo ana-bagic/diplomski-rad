18db9b80cfdaf96fe6ff0a808322eb3c3549e6e2
Ana Bagić <ana.bagic13@gmail.com>
2024-07-30 09:37:07 +0200

    Refactored images and added bravura font

--- code/src/main/java/pianolearn/diplomskirad/constants/Fonts.java
+++ code/src/main/java/pianolearn/diplomskirad/constants/Fonts.java
@@ -11,15 +11,19 @@ public class Fonts {
     public static final Font micro;
     public static final Font error = Font.font("Arial", 20);
     public static final Font key = Font.font("Arial", 12);
+    public static final Font music;
 
     static {
-        String resource = "/fonts/retro-osar.ttf";
-        Font.loadFont(Fonts.class.getResourceAsStream(resource), 12);
+        String retroOsarFont = "/fonts/retro-osar.ttf";
+        Font.loadFont(Fonts.class.getResourceAsStream(retroOsarFont), 12);
+        String bravuraFont = "/fonts/bravura.ttf";
+        Font.loadFont(Fonts.class.getResourceAsStream(bravuraFont), 12);
 
         title = Font.font("Retro Osar", 100);
         header = Font.font("Retro Osar", 50);
         body = Font.font("Retro Osar", 24);
         tooltip = Font.font("Retro Osar", 18);
         micro = Font.font("Retro Osar", 18);
+        music = Font.font("Bravura Text", 50);
     }
 }
--- code/src/main/java/pianolearn/diplomskirad/constants/Images.java
+++ code/src/main/java/pianolearn/diplomskirad/constants/Images.java
@@ -6,11 +6,13 @@ import java.util.Objects;
 
 public class Images {
 
-    public static final Image uploadIcon = getImage("upload.png");
-    public static final Image musicLibraryIcon = getImage("music-library.png");
-    public static final Image settingsIcon = getImage("settings.png");
+    private static final String iconsPath = "icons/";
 
-    public static final Image backArrowIcon = getImage("back-arrow.png");
+    public static final Image uploadIcon = getImage(iconsPath + "upload.png");
+    public static final Image musicLibraryIcon = getImage(iconsPath + "music-library.png");
+    public static final Image settingsIcon = getImage(iconsPath + "settings.png");
+
+    public static final Image backArrowIcon = getImage(iconsPath + "back-arrow.png");
 
     public static final Image beethovenImage = getImage("beethoven.jpg");
 
