f76760b233a383bc7a9386a7f389879fdba2a793
Ana Bagić <ana.bagic13@gmail.com>
2024-09-02 18:07:50 +0200

    Minor fix

--- code/src/main/java/pianolearn/diplomskirad/view/screens/PlayView.java
+++ code/src/main/java/pianolearn/diplomskirad/view/screens/PlayView.java
@@ -17,6 +17,9 @@ public class PlayView extends BaseNavigationView {
 
     private void setupView() {
         topStackPane.getChildren().add(playToolbarView);
+
+        playToolbarView.setMinWidth(HBox.USE_PREF_SIZE);
+        playToolbarView.setMaxWidth(HBox.USE_PREF_SIZE);
     }
 
     public void setUsesBothHands(boolean usesBothHands) {
